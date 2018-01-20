cf0318d09f2e5a7fa3eb3252e240699dc8734cae
Pavel Radchuk <pradchuk2@mail.greenriver.edu>
2018-01-19 22:21:46 -0500

    index complete

--- index.php
+++ index.php
@@ -5,3 +5,21 @@
  * Date: 1/18/18
  * Time: 7:18 PM
  */
+ //require the autoload file
+ require_once ('vendor/autoload.php');
+
+ //create an instance of the Base class
+ $f3 = Base::instance();
+
+ //set debug level
+ $f3->set('DEBUG', 3);
+
+ //define a default route
+ $f3->route("GET /", function() {
+     $view = new view();
+     echo $view->render('pages/home.html');
+ }
+ };
+
+//run Fat-Free
+$f3->run();
