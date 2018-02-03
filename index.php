5bd34ebc7938fd10a2c1f1fa4148c5a83a5797a6
Pavel Radchuk <pradchuk2@mail.greenriver.edu>
2018-02-03 00:14:58 -0500

    almost there

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 * Date: 1/18/18
 * Time: 7:18 PM
 */
+
+session_start();
+
 //require the autoload file
 require_once ('vendor/autoload.php');
 
@@ -14,26 +17,107 @@ $f3 = Base::instance();
 //set debug level
 $f3->set('DEBUG', 3);
 
-//define a default route
-$f3->route("GET /", function() {
-$view = new view();
-echo $view->render('pages/home.html');
-});
+$f3->set('states', array("Washington","Idaho","California","Oregon"));
 
-$f3 -> route('GET/POST /information', function($f3) {
-$view = new View;
-echo $view->render('pages/information.html');
-});
+$f3->set('indoors', array( "tv", "movies", "cooking", "board games", "puzzles", "reading", "playing cards", "video games"));
+$f3->set('outdoors', array( "hiking", "biking", "swimming", "collecting", "walking", "climbing"));
 
-$f3 -> route('GET /profile', function($f3) {
-    $view = new View;
-    echo $view->render('views/profile.html');
-});
+$f3->route('GET /', function() {
+            $template = new Template();
+            echo $template->render('pages/home.html');
+        }
+    );
 
-$f3 -> route('GET /interests', function($f3) {
-    $view = new View;
-    echo $view->render('views/interests.html');
-});
+    $f3->route('GET|POST /information', function($f3, $params) {
+            if(isset($_POST['submit'])){
+                $first = $_POST['first'];
+                $last = $_POST['last'];
+                $age = $_POST['age'];
+                $gender = $_POST['gender'];
+                $phone = $_POST['phone'];
+                $_SESSION['first'] = $first;
+                $_SESSION['last'] = $last;
+                $_SESSION['age'] = $age;
+                $_SESSION['gender'] = $gender;
+                $_SESSION['phone'] = $phone;
+                include('model/validate.php');
+                $f3->set('first', $first);
+                $f3->set('last', $last);
+                $f3->set('age', $age);
+                $f3->set('gender', $gender);
+                $f3->set('phone', $phone);
+                $f3->set('errors', $errors);
+                $f3->set('success', $success);
+            }
+            $template = new Template();
+            echo $template->render('views/information.html');
+            if($success) {
+                $f3->reroute('/profile');
+            }
+        }
+    );
 
-//run Fat-Free
-$f3->run();
+    $f3->route('GET|POST /profile', function($f3, $params) {
+            if(isset($_POST['submit'])){
+                $email = $_POST['email'];
+                $state = $_POST['state'];
+                $bio = $_POST['bio'];
+                $seeking = $_POST['seeking'];
+                $_SESSION['email'] = $email;
+                $_SESSION['state'] = $state;
+                $_SESSION['bio'] = $bio;
+                $_SESSION['seeking'] = $seeking;
+                include('model/validate.php');
+                $f3->set('email', $email);
+                $f3->set('state', $state);
+                $f3->set('bio', $bio);
+                $f3->set('seeking', $seeking);
+                $f3->set('errors', $errors);
+                $f3->set('success', $success);
+            }
+            $template = new Template();
+            echo $template->render('views/profile.html');
+            if($success) {
+                $f3->reroute('/interests');
+            }
+        }
+    );
+    //Define a form 3 route
+    $f3->route('GET|POST /interests', function($f3, $params) {
+            if(isset($_POST['submit'])){
+                $indoor = $_POST['indoors'];
+                $outdoor = $_POST['outdoors'];
+                $_SESSION['indoor'] = $indoor;
+                $_SESSION['outdoor'] = $outdoor;
+                include('model/validate.php');
+                $f3->set('indoor', $indoor);
+                $f3->set('outdoor', $indoor);
+                $f3->set('errors', $errors);
+                $f3->set('success', $success);
+            }
+            $template = new Template();
+            echo $template->render('pages/interests.html');
+            if($success) {
+                $f3->reroute('/summary');
+            }
+        }
+    );
+    $f3->route('GET|POST /summary', function($f3, $params) {
+            $f3->set('first', $_SESSION['first']);
+            $f3->set('last', $_SESSION['last']);
+            $f3->set('age', $_SESSION['age']);
+            $f3->set('gender', $_SESSION['gender']);
+            $f3->set('phone', $_SESSION['phone']);
+            $f3->set('email', $_SESSION['email']);
+            $f3->set('state', $_SESSION['state']);
+            $f3->set('seeking', $_SESSION['seeking']);
+            $f3->set('indoor', $_SESSION['indoor']);
+            $f3->set('outdoor', $_SESSION['outdoor']);
+            $f3->set('bio', $_SESSION['bio']);
+            $template = new Template();
+            echo $template->render('views/summary.html');
+        }
+    );
+
+    // run fat free
+    $f3->run();
