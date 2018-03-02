013af073ea6bd75f1b9aa8edf2e5b8185f66bbdb
Pavel Radchuk <pradchuk2@mail.greenriver.edu>
2018-03-02 12:21:08 -0500

    database

--- model/database.php
+++ model/database.php
@@ -2,6 +2,7 @@
 require '/home/pradchuk/config.php';
 class database
 {
+    protected $dbh;
     /**
      * Member constructor.
      * connecting to database
@@ -9,14 +10,38 @@ class database
     function __construct()
     {
         try {
-            $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+            $this->dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
             echo "connected to database!";
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
-    function message()
+    function insertMember()
     {
-        echo "complete testing";
+        $sql= "INSERT INTO members(fname, lname, age, gender, phone, email, state, seeking, bio, premium, image, interests)
+        VALUES(:fname, :lname, :age, :gender, :phone, :email, :state, :seeking, :bio, :premium, :image, :interests)";
+
+        //Prepare the statement
+        $statement = $this->dbh->prepare($sql);
+
+        //Bind the parameters
+        $statement->bindParam(':fname', $fname, PDO::PARAM_STR);
+        $statement->bindParam(':lname', $lname, PDO::PARAM_STR);
+        $statement->bindParam(':age', $age, PDO::PARAM_INT);
+        $statement->bindParam(':gender', $gender, PDO::PARAM_STR);
+        $statement->bindParam(':phone', $phone, PDO::PARAM_STR);
+        $statement->bindParam(':email', $email, PDO::PARAM_STR);
+        $statement->bindParam(':state', $state, PDO::PARAM_STR);
+        $statement->bindParam(':seeking', $seeking, PDO::PARAM_STR);
+        $statement->bindParam(':bio', $bio, PDO::PARAM_STR);
+        $statement->bindParam(':premium', $premium, PDO::PARAM_INT);
+        $statement->bindParam(':image', $image, PDO::PARAM_STR);
+        $statement->bindParam(':interests', $interests, PDO::PARAM_STR);
+
+        //Execute
+        $statement->execute();
+        $id = $this->dbh->lastInsertId();
+        echo "<p>Pet $id inserted successfully.</p>";
+
     }
 }
